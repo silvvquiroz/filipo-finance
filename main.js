9d7300010a22ba125a19c233919282843e46b455
squiroz <silvana.quiroz@pucp.edu.pe>
2025-04-11 15:09:09 -0500

    agregar la conexión con el back y los cards de ahorros

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const fs = require('fs');
 
 let mainWindow;
 
@@ -18,4 +19,66 @@ app.whenReady().then(() => {
     });
 
     mainWindow.loadFile('index.html');
+});
+
+/************************* JSON logic **************************/
+
+// ruta al archivo json
+const ahorrosPath = path.join(__dirname, 'data', 'ahorros.json');
+const catPath = path.join(__dirname, 'data', 'categorias.json');
+const gastosPath = path.join(__dirname, 'data', 'gastos.json');
+const ingresosPath = path.join(__dirname, 'data', 'ingresos.json');
+
+// leer el archivo
+function leerAhorros() {
+    const datos = fs.readFileSync(ahorrosPath, 'utf-8');
+    return JSON.parse(datos);
+}
+
+function leerCategorias() {
+    const datos = fs.readFileSync(catPath, 'utf-8');
+    return JSON.parse(datos);
+}
+
+function leerGastoss() {
+    const datos = fs.readFileSync(gastosPath, 'utf-8');
+    return JSON.parse(datos);
+}
+
+function leerIngresos() {
+    const datos = fs.readFileSync(ingresosPath, 'utf-8');
+    return JSON.parse(datos);
+}
+
+
+// guardar datos
+function guardarAhorros(nuevosDatos) {
+    fs.writeFileSync(ahorrosPath, JSON.stringify(nuevosDatos, null, 2), 'utf-8');
+}
+
+function guardarCategorias(nuevosDatos) {
+    fs.writeFileSync(catPath, JSON.stringify(nuevosDatos, null, 2), 'utf-8');
+}
+
+function guardarGastos(nuevosDatos) {
+    fs.writeFileSync(gastosPath, JSON.stringify(nuevosDatos, null, 2), 'utf-8');
+}
+
+function guardarIngresos(nuevosDatos) {
+    fs.writeFileSync(ingresosPath, JSON.stringify(nuevosDatos, null, 2), 'utf-8');
+}
+
+
+
+/********************************* IPC MAIN **********************/
+
+// escuchar cuando el front pide los datos
+ipcMain.handle('obtener-ahorros', ()=> {
+    return leerAhorros();
+});
+
+
+// escuchar cuando el front manda datos nuevos
+ipcMain.handle('guardar-ahorros', (event, nuevosDatos) => {
+    guardarAhorros(nuevosDatos);
 });
--- renderer.js
+++ renderer.js
@@ -1,1 +1,22 @@
 const { ipcRenderer } = require('electron');
+
+// pedir datos al backend
+async function cargarAhorros() {
+    // cargar los ahorros
+    const ahorros = await ipcRenderer.invoke('obtener-ahorros');
+
+    // llamar al contenedor de html
+    const contenedor = document.getElementById('ahorros-container');
+    contenedor.innerHTML = '';
+
+    // renderizar el card para cada ahorro
+    ahorros.forEach(ahorro => {
+        const div = document.createElement('div');
+        div.className = 'ahorro-card';
+        div.innerText = `${ahorro.nombre}: S/. ${ahorro.metaSoles.toFixed(2)}`;
+        contenedor.appendChild(div);
+    });
+
+}
+
+//cargarAhorros();
